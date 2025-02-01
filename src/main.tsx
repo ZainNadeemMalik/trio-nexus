3de39edadf8188a938fd98bfeb1e64cdc5f4ee85
Zain Malik <zainnadeemmalik568@gmail.com>
2025-02-01 17:38:18 +0500

    add redirects file to project add page title to index.html change routes page address in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Routes } from './routes/routes.tsx'
+import { Routes } from './routes/Routes.tsx'
 import './index.css'
 
 const router = createBrowserRouter(Routes);
