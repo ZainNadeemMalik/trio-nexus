96b742f0dda069411cf406a8c249e5742f88c4c3
Zain Malik <zainnadeemmalik568@gmail.com>
2025-02-04 19:24:09 +0500

    add nav to homepage
    rearrane hero section design of homepage
    
    add blog section to homepage
    
    add footer to homepage

--- src/components/Header/Footer/Footer.tsx
+++ src/components/Header/Footer/Footer.tsx
@@ -6,6 +6,21 @@ export const Footer = () => {
   <footer className={styles.footer}>
     <p>www.trionexus.com</p>
     <p>The Ultimate Nexus for Legal, Educational, and IT Services</p>
+
+    <div className={styles.footerColumn}>
+
+      <div className={styles.footerContact}>
+        <p>+92-000-000000-0</p>
+        <p>trionexus4@gmail.com</p>
+    </div>
+
+    <div className={styles.footerSocials}>
+      <div><a href="#"><img src="" alt="" /></a>facebook-icon</div>
+      <div><a href="#"><img src="" alt="" /></a>instagram-icon</div>
+      <div><a href="#"><img src="" alt="" /></a>gmail-icon</div>
+    </div>
+
+    </div>
   </footer>
  )
 }
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom"
 import styles from "./HomePage.module.css"
 import triologo from '../../assets/triologo.jpg';
+import { Footer } from "../../components/Header/Footer/Footer";
+import triolawpagephoto from "../../assets/triolawpagephoto.jpg"
 
 export const HomePage = () => {
   return (
@@ -17,22 +19,84 @@ export const HomePage = () => {
 </div>
 </section>
 
+    <nav className={styles.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/law">Legal</Link>
+        <Link to="/education">Education</Link>
+        <Link to="/tech">IT</Link>
+      </nav>
+
 
       {/* <p className={styles.serviceText}>What we can do for you:</p> */}
 
       <ul className={styles.servicesList}>
         <li className={styles.law}>
-          <Link to="/law">Legal<span>Services</span></Link>
+          <Link to="/law">Legal - Services
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, saepe!</p>
+          </Link>
         </li>
 
         <li className={styles.education}>
-          <Link to="/education"><span>Education</span>Services</Link>
+          <Link to="/education">Education - Services 
+          
+          </Link>
         </li>
 
         <li className={styles.tech}>
-          <Link to="/tech"> <span>IT</span>Services</Link>
+          <Link to="/tech">Technological - Services
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, perspiciatis?</p>
+          </Link>
         </li>
       </ul>
+
+{/* ---------------BLOG ARTICLES---------------  */}
+{/* all these articles need to be generated programatically like the cart items via an articles component */}
+
+      <section className={styles.blogArticles}>
+
+        <h2 className={styles.blogArticlesTitle}>Our Blog Articles</h2>
+
+        <div className={styles.blogArticle}>
+
+          <div className={styles.blogArticleImageContainer}>
+            <img src={triolawpagephoto} alt="article-image" />
+          </div>
+
+          <div className={styles.blogArticleText}>
+            <h3>Title</h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+          </div>
+
+        </div>
+
+        <div className={styles.blogArticle}>
+
+      <div className={styles.blogArticleImageContainer}>
+        <img src={triolawpagephoto} alt="article-image" />
+      </div>
+
+      <div className={styles.blogArticleText}>
+        <h3>Title</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+      </div>
+
+      </div>
+
+  <div className={styles.blogArticle}>
+
+    <div className={styles.blogArticleImageContainer}>
+      <img src={triolawpagephoto} alt="article-image" />
+    </div>
+
+    <div className={styles.blogArticleText}>
+      <h3>Title</h3>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+    </div>
+  </div>
+
+      </section>
+
+      <Footer />
     </div>
   )
 }
