b4ba83fc917d8637478319567c4859b3c9d51d66
Zain Malik <zainnadeemmalik568@gmail.com>
2025-02-05 18:30:12 +0500

    make navbar sticky
    create new layouts for blogs
    
    create new layouts for services
    
    add details and new info to footer

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,6 +3,8 @@ import styles from "./HomePage.module.css"
 import triologo from '../../assets/triologo.jpg';
 import { Footer } from "../../components/Header/Footer/Footer";
 import triolawpagephoto from "../../assets/triolawpagephoto.jpg"
+import fameFoundationLogo from "../../assets/fame-foundation-logo.jpg"
+import techlogo from "../../assets/tech-logo.jpg"
 
 export const HomePage = () => {
   return (
@@ -49,13 +51,60 @@ export const HomePage = () => {
         </li>
       </ul>
 
+      <ul className={styles.servicesList}>
+
+        <li className={styles.law2}>
+          <Link to="/law">
+          <div className={styles.lawImageContainer}>
+            <img src={triologo} alt="trio-law-logo" />
+          </div>
+
+          <div className={styles.lawText}>
+            <h3>Legal - Services</h3>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, saepe!</p>
+          </div>
+
+          </Link>
+        </li>
+
+        <li className={styles.education2}>
+          <Link to="/education">
+          <div className={styles.educationImageContainer}>
+            <img src={fameFoundationLogo} alt="fame-logo" />
+          </div>
+
+          <div className={styles.educationText}>
+            <h3>Education - Services</h3>
+            
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, molestiae?</p>
+          </div>
+          </Link>
+        </li>
+
+        <li className={styles.tech2}>
+          <Link to="/tech">
+
+          <div className={styles.techImageContainer}>
+            <img src={techlogo} alt="tech-logo" />
+          </div>
+
+          <div className={styles.techText}>
+            <h3>Technological - Services </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, perspiciatis?</p>
+          </div>
+
+          </Link>
+        </li>
+      </ul>
+
 {/* ---------------BLOG ARTICLES---------------  */}
 {/* all these articles need to be generated programatically like the cart items via an articles component */}
 
       <section className={styles.blogArticles}>
 
         <h2 className={styles.blogArticlesTitle}>Our Blog Articles</h2>
-
+<div className={styles.blogArticleWrapper}>
         <div className={styles.blogArticle}>
 
           <div className={styles.blogArticleImageContainer}>
@@ -93,6 +142,62 @@ export const HomePage = () => {
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
     </div>
   </div>
+
+  
+  <div className={styles.blogArticle}>
+
+<div className={styles.blogArticleImageContainer}>
+  <img src={triolawpagephoto} alt="article-image" />
+</div>
+
+<div className={styles.blogArticleText}>
+  <h3>Title</h3>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+</div>
+
+  </div>
+</div>
+
+<div className={styles.blogArticlesWrapper2}>
+<div className={styles.blogArticle}>
+
+<div className={styles.blogArticleImageContainer}>
+  <img src={triolawpagephoto} alt="article-image" />
+</div>
+
+<div className={styles.blogArticleText}>
+  <h3>Title</h3>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+</div>
+
+</div>
+
+<div className={styles.blogArticle}>
+
+<div className={styles.blogArticleImageContainer}>
+  <img src={triolawpagephoto} alt="article-image" />
+</div>
+
+<div className={styles.blogArticleText}>
+  <h3>Title</h3>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+</div>
+
+</div>
+
+<div className={styles.blogArticle}>
+
+<div className={styles.blogArticleImageContainer}>
+  <img src={triolawpagephoto} alt="article-image" />
+</div>
+
+<div className={styles.blogArticleText}>
+  <h3>Title</h3>
+  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, quod.</p>
+</div>
+
+</div>
+</div>
 
       </section>
 
